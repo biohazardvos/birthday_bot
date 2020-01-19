ab79778280308ba03ef680c13f04dbc7f34a60b6
Anton Bragin <abragin@iponweb.net>
2020-01-19 13:19:29 +0300

    add test xmlrunner

--- test_birthdaybot.py
+++ test_birthdaybot.py
@@ -89,4 +89,7 @@ class TestBirthdaybot(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
     unittest.main()
